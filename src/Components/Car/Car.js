3e50a50fcea0b14fa2fc61c9c33523724d34b524
Blake Lamb <blake.lamb31@gmail.com>
2019-07-08 12:21:18 -0600

    styled the individual car view

--- src/Components/Car/Car.js
+++ src/Components/Car/Car.js
@@ -7,6 +7,8 @@ import Axios from 'axios'
 
 import {getCar, addMiles, gotOilChange,deleteCar} from '../../redux/reducers/cars'
 
+import './car.css'
+
 class Car extends Component {
     constructor(props) {
         super(props)
@@ -43,11 +45,9 @@ class Car extends Component {
             async function send() {
                 let name = `${car.driver_first_name} ${car.driver_last_name}`
                 let adminName = `${car.admin_first_name} ${car.admin_last_name}`
-                console.log("Miles: ", updatedMiles - last_oil_change, "Car", car, "Admin Name", adminName)
                 let message = `Hello, ${name}. Your car, ${car.car_color} ${car.car_year} ${car.car_make} ${car.car_model}, is now at ${updatedMiles} miles (${updatedMiles - last_oil_change} miles since the oil was last changed). Please get the oil changed and the tires rotated and then contact your fleet administrator, ${adminName}`
                 let res = Axios.post('/api/sendSMS', { adminName, message })
                 await alert(`Message has been sent to notify ${name} of the needed maintenance`)
-                console.log(res)
                 // setName('')
                 // setMessage('')
               }
@@ -62,58 +62,79 @@ class Car extends Component {
 
     render() {
         let {car} = this.props
-        console.log(232332, this.props)
         return(
-            <div>
-                {this.props.user ?
-                    <div>
-                    {this.props.car && 
+            <div className='singleCarOuterDiv'>
+                {/* {this.props.user ? */}
+                {this.props ?
+                
+                    <div className='outerCarViewDiv'>
+                        {this.props.car && 
                         <div>
-                            
-                            <h3>{`${car.car_year} ${car.car_make} ${car.car_model} (${car.car_color})`}</h3>
-                            <img width={200} src={`${car.car_img}`} />
-                            <p>{`${car.car_address}`} </p>
-                            <p>{`${car.car_city}, ${car.car_state} ${car.car_zip_code}`}</p>
-                            <p>{`Current Mileage: ${car.car_mileage}`}</p>
-                            <p>{`Miles At Last Oil Change : ${car.last_oil_change}`}</p>
-                            <p>{`Mileage Since Last Oil Change: ${car.car_mileage - car.last_oil_change}`}</p>
-                            <button onClick={() => this.handleGotOilChange(car.car_mileage)}>Got Oil Change</button>
-                            <input 
-                            min={0}
-                            placeholder='Add Miles Here' 
-                            name='addMiles' 
-                            value={this.state.addMiles}
-                            type='number'
-                            onChange={event => this.handleChange(event)} />
-                            <button onClick={() => this.handleAddMiles(car.car_mileage, car.last_oil_change, car)}>Add Miles</button>
-                            {car.driver_id &&
-                            <div>
-                                <h3>{`Driver: ${car.driver_first_name} ${car.driver_last_name}`}</h3>
-                                <img width={200} src={`${car.driver_img}`} />
-                                <p>{`Driver ID: ${car.driver_id}`}</p>
-                            </div>
-                            }
-                            {(car.car_mileage - car.last_oil_change >= 5000) &&
-                            <div>
-                                <h3>Mainenance Needed:</h3>
-                                <ul>
-                                    <li>Oil Change</li>
-                                    <li>Tire Rotation</li>
-                                </ul>
-                            </div>
+                            <div className='carViewDiv'>
+                                
+                                <h3 id='indCarTitle'>{`${car.car_year} ${car.car_make} ${car.car_model} (${car.car_color})`}</h3>
+                                <img id='indCarPic'  src={`${car.car_img}`} />
+                                <div id='indCarAddress'>
+                                    <p>{`${car.car_address}`} </p>
+                                    <p>{`${car.car_city}, ${car.car_state} ${car.car_zip_code}`}</p>
+                                </div>
+                                <div id='indCarMileageInfo'>
+                                    <p className='shrinkText'>{`Current Mileage: ${car.car_mileage}`}</p>
+                                    <p id='middleRow' className='shrinkText'>{`Miles At Last Oil Change : ${car.last_oil_change}`}</p>
+                                    <div id='indCarOilChangeInfo'>
+                                        <p className='shrinkText'>{`Miles Since Oil Change: ${car.car_mileage - car.last_oil_change}`}</p>
+                                        <button className='shrinkText indCarButtonMarg' onClick={() => this.handleGotOilChange(car.car_mileage)}>Oil Changed</button>
+                                    </div>
+                                    <div className='inputMilesDiv'>
+                                        <input 
+                                        min={0}
+                                        placeholder='Add Miles Here' 
+                                        name='addMiles' 
+                                        value={this.state.addMiles}
+                                        type='number'
+                                        onChange={event => this.handleChange(event)} />
+                                        <button className='indCarButtonMarg' onClick={() => this.handleAddMiles(car.car_mileage, car.last_oil_change, car)}>Add Miles</button>
 
-                            }
-                            <button onClick={() => window.history.back()}>Back</button>
-                            <button onClick={() => this.props.history.push(`/user/admin/api/cars/edit/${car.car_id}`)}>Edit</button>
-                            <button onClick={() => this.handleDeleteCar(this.props.match.params.id)}>Delete Car</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='indCarDriverDiv'>
+                                {car.driver_id &&
+                                    <div className='indCarDriverInfo'>
+                                        <h3>{`Driver: ${car.driver_first_name} ${car.driver_last_name}`}</h3>
+                                        <img width={200} src={`${car.driver_img}`} />
+                                        <p>{`Driver ID: ${car.driver_id}`}</p>
+                                    </div>
+                                    }
+                                    {(car.car_mileage - car.last_oil_change >= 5000) &&
+                                    <div className='indCarMaintenenceDiv'>
+                                        <h3>Mainenance Needed:</h3>
+                                        <ul>
+                                            <li>Oil Change</li>
+                                            <li>Tire Rotation</li>
+                                        </ul>
+                                    </div>
+
+
+                                    }
+                                    <div className='indCarButtons'>
+                                        <button onClick={() => window.history.back()}>Back</button>
+                                        <button className='indCarMiddleButton' onClick={() => this.props.history.push(`/user/admin/api/cars/edit/${car.car_id}`)}>Edit Car</button>
+                                        <button onClick={() => this.handleDeleteCar(this.props.match.params.id)}>Delete Car</button>
+
+                                    </div>
+
+                            </div>
+                            
                         </div>
-                    }
+                        }
                     </div>
                     :
 
                     <Redirect to='/' />
                 }
             </div>
+                    
         )
     }
 }
